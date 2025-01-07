c3f35d37184a3d9f0ae9520f2049d28eb98cd4bf
namik2 <qocayevn170@gmail.com>
2025-01-07 14:08:21 +0300

    first-commit
    first-commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TEST</title>
+</head>
+<body>
+    <section>
+        <form action="#">
+            first name <input type="checkbox" name="" id="">
+            last name <input type="checkbox" name="" id="">
+            <input type="submit" value="submit">
+            
+            <input type="reset" value="reset">
+        </form>
+    </section>
+</body>
+</html>
